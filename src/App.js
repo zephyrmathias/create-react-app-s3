62976b5e21eb6c98c111a635653b24ec7832a612
Wuttichai Junsoontraporn <wuttichai.junsoontraporn@gmail.com>
2020-04-11 12:59:37 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Staging
+          Dispatch on a specific branch
         </a>
       </header>
     </div>
