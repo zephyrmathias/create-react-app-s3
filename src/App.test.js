ef21d3f960947cff56c0b3e292e4e278097c96d8
Wuttichai Junsoontraporn <wuttichai.junsoontraporn@gmail.com>
2020-04-11 12:59:54 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react staging/i);
+  const linkElement = getByText(/Dispatch on a specific branch/i);
   expect(linkElement).toBeInTheDocument();
 });
