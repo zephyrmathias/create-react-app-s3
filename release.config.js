2e70e7a2360ca5810e9784edad026546e98e141e
Wuttichai Junsoontraporn <wuttichai.junsoontraporn@gmail.com>
2020-04-11 16:29:21 +0700

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -3,15 +3,23 @@ module.exports = {
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
+    "@semantic-release/npm",
     "@semantic-release/github",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
+    ],
     [
       "@semantic-release/git",
       {
         "assets": [
           "docs/CHANGELOG.md",
+          "package.json",
+          "package-lock.json"
         ],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        "message": "chore(release): ${nextRelease.version}\n${nextRelease.notes}"
       }
     ]
   ]
