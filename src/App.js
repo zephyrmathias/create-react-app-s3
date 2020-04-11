e8db14d6c9d1a8f8e290eede004415e4db17adfd
Wuttichai Junsoontraporn <wuttichai.junsoontraporn@gmail.com>
2020-04-11 17:34:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test Semantic Release
+          Test Staging Hook
         </a>
       </header>
     </div>
