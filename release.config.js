535ac2b8fb3e49eb34df83f51b1363a6abc6cb51
Wuttichai Junsoontraporn <wuttichai.junsoontraporn@gmail.com>
2020-04-11 16:49:29 +0700

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -16,6 +16,8 @@ module.exports = {
       {
         "assets": [
           "docs/CHANGELOG.md",
+          "package.json",	
+          "package-lock.json"
         ],
         "message": "chore(release): ${nextRelease.version}\n${nextRelease.notes}"
       }
