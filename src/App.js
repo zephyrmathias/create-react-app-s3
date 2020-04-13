57cf260ffa7dddb4d673ca730d1b63f40b6e2fe9
Wuttichai Junsoontraporn <wuttichai.junsoontraporn@gmail.com>
2020-04-13 12:58:07 +0700

    feat(cicd): test ci/cd
    * feat(cicd): test ci/cd
    
    * feat(cicd): test ci/cd

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test Semantic Release
+          Test CI/CD
         </a>
       </header>
     </div>
